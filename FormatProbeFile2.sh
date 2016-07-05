d6ee8c3dc45b0c25a59dae68d43d81e50f9badd6
ChristianFougner <c.l.fougner@studmed.uio.no>
2016-07-05 18:41:19 +0200

    Reordered to folders

--- FormatProbeFile2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-INPUT_FILE='/Users/Christian/Documents/Forskerlinja/DMBA-indusert/Sequencing/ReferenceFiles/S0276129/OnlyCoordinatesAndID.txt'
-
-OUTPUT_FILE='/Users/Christian/Documents/Forskerlinja/DMBA-indusert/Sequencing/ReferenceFiles/S0276129/OnlyCoordinatesAndID.gawk.txt'
-
-gawk 'program' $INPUT_FILE > OUTPUT_FILE
